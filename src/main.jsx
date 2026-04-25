c82c7743a4e19fd3dbd8d8b6a8f1ae01481e788d
lilyaaas <ilyasslhouari@gmail.com>
2026-04-26 00:19:10 +0100

    style: add missing import for main.css in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
 import App from './App.jsx'
+import './style/main.css'
 import './i18n'
 
 createRoot(document.getElementById('root')).render(
